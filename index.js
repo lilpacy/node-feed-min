e5dbcfbadb79979a5d66b0421866cb9d79aba77a
LilPacy <revivedtomorrow@gmail.com>
2021-01-24 19:03:46 +0900

    ログにrssをそれぞれの規格でアウトプット

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 const fd = require("feed");
-console.log(new fd.Feed);
+
+const feed = new fd.Feed({
+  title: "Feed Title",
+  description: "This is my personal feed!",
+  id: "http://example.com/",
+  link: "http://example.com/",
+  language: "en", // optional, used only in RSS 2.0, possible values: http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
+  image: "http://example.com/image.png",
+  favicon: "http://example.com/favicon.ico",
+  copyright: "All rights reserved 2013, John Doe",
+  updated: new Date(2013, 6, 14), // optional, default = today
+  generator: "awesome", // optional, default = 'Feed for Node.js'
+  feedLinks: {
+    json: "https://example.com/json",
+    atom: "https://example.com/atom"
+  },
+  author: {
+    name: "John Doe",
+    email: "johndoe@example.com",
+    link: "https://example.com/johndoe"
+  }
+});
+console.log("rss2\n",feed.rss2(),"\n");
+console.log("atom1\n",feed.atom1(),"\n");
+console.log("json1\n",feed.json1(),"\n");
